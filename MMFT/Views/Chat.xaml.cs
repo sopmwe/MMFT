31832d803327ece57fd319043e87beb62ade1b37
Franz <mc.geek47.19@gmail.com>
2026-09-02 16:49:01 +0200

    Erste Änderungen an Chat: Liste der Kontakte zusammen mit 4 Item Templates für Kontakten, sowie 4 Beispiele hinterlegt. Ressourcen: Standardpfp.png hinzugefügt.

--- MMFT/Views/Chat.xaml.cs
+++ MMFT/Views/Chat.xaml.cs
@@ -20,6 +20,66 @@ namespace MMFT.Views
         public Chat()
         {
             InitializeComponent();
+
+            var items = new List<ListBoxItemModel>
+                {
+                     new ListBoxItemModel { ImagePath = "pack://application:,,,/Ressourcen/Standardpfp.png", Title = "Maren", TemplateTyp = ItemTyp.Standard },
+                     new ListBoxItemModel { ImagePath = "pack://application:,,,/Ressourcen/Standardpfp.png", Title = "Milena", TemplateTyp = ItemTyp.MitZahl, Anzahl = 5 },
+                     new ListBoxItemModel { ImagePath = "pack://application:,,,/Ressourcen/Standardpfp.png", Title = "Tim", TemplateTyp = ItemTyp.StandardStatus, Subtitle = "Ich benutze WhatsApp!" },
+                     new ListBoxItemModel { ImagePath = "pack://application:,,,/Ressourcen/Standardpfp.png", Title = "Franz", TemplateTyp = ItemTyp.StatusMitZahl, Subtitle = "Schaut mal, was ich gemacht habe!", Anzahl = 3 }
+                };
+
+            Kontakte.ItemsSource = items;
         }
+
+        
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        
+    }
+
+    public enum ItemTyp
+    {
+        Standard,
+        MitZahl,
+        StandardStatus,
+        StatusMitZahl
+
+    }
+    public class KontakttypSelector : DataTemplateSelector
+    {
+        public DataTemplate StandardTemplate { get; set; }
+        public DataTemplate ZahlTemplate { get; set; }
+        public DataTemplate StandardStatusTemplate { get; set; }
+
+        public DataTemplate ZahlStatusTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is ListBoxItemModel model)
+            {
+                if (model.TemplateTyp == ItemTyp.MitZahl)
+                    return ZahlTemplate;
+                else if (model.TemplateTyp == ItemTyp.Standard)
+                    return StandardTemplate;
+                else if (model.TemplateTyp == ItemTyp.StatusMitZahl)
+                    return ZahlStatusTemplate;
+                else
+                    return StandardStatusTemplate;
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+
+    public class ListBoxItemModel
+    {
+        public string ImagePath { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public ItemTyp TemplateTyp { get; set; }
+        public int? Anzahl { get; set; }
     }
 }
