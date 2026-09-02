6e1cdf92e277a1fe475361e8b646bdf74e6752bf
Milena Wagner <milena.wagner.steinberg@gmail.com>
2026-09-02 10:06:36 +0200

    Added TCP-Entgegennehmen-Grundstruktur ohne Fehlermeldungen, Pakete können entgegengenommen werden, aber ohne Aktion grade und  Pakete auch noch nicht gesendet. LG Milena

---
+++ MMFT/TCP/Verarbeiter.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+
+namespace Verarbeiter
+{
+    public class Verarbeiter
+    {
+        public static void VerarbeiteTyp1(TcpClient client, string typ)
+        {
+            //hier kommt noch rein was mit den Daten passiereb soll
+            client.Close(); //Verbindung zum Client am Ende schließen
+        }
+        public static void VerarbeiteTyp2(TcpClient client, string typ)
+        {
+            //auch die Logik noch
+            client.Close();
+        }
+        public static void VerarbeiteTyp3(TcpClient client, string typ)
+        {
+            //ja hier auch Logik nh
+            client.Close();
+        }
+    }
+}
