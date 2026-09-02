940925a059962335cfb258ade4fdef46be2cc2c7
Maren Wenz <maren-we1@web.de>
2026-09-02 17:09:15 +0200

    added: Bildauswahl und Defaultbild eingestellt

--- MMFT/Views/FirstAccessLogin.xaml.cs
+++ MMFT/Views/FirstAccessLogin.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,19 +21,61 @@ namespace MMFT.Views
     /// </summary>
     /// 
 
-    //Standardbild einstellen
-    //Bildauswahl ermöglichen
+    //Passwortanforderungen??
     //Bild in DB speichern
-    //PW verdecken
-    //PW und Nutzernamen speichern in DB
+    //PW und Nutzernamen speichern in DB, PreparedStatements verwenden
     //
-    public partial class FirstAccessLogin : Window
+    public partial class FirstAccessLogin : Window, INotifyPropertyChanged
     {
-        public FirstAccessLogin()
+        private const string DefaultProfilbild = "pack://application:,,,/MMFT;component/Ressourcen/Profilbild.png";
+
+        private string _profilbild;
+
+        public string Profilbild
         {
-            InitializeComponent();
+            get => _profilbild;
+            set
+            {
+                if (_profilbild != value)
+                {
+                    _profilbild = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
-    
+        public FirstAccessLogin()
+        {
+            Profilbild = DefaultProfilbild;
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BtnEinfuegen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Bilddateien (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg|Alle Dateien (*.*)|*.*",
+                Title = "Profilbild auswählen"
+            };
+
+            if (dialog.ShowDialog() == true)
+            { 
+                Profilbild = dialog.FileName;
+            }
+        }
+
+        private void BtnLoeschen_Click(object sender, RoutedEventArgs e)
+        {
+
+            Profilbild = DefaultProfilbild;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
+
 }
