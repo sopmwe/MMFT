b89b359d56f4549744821f1dc2f1173fe501c8d2
TimKlm679 <tim.klimmer@gmail.com>
2026-09-03 10:55:30 +0200

    Added: Nachrichten werden jz RSA verschluesselt in der DB gespeichert und RSA verschluesselt als Paket gemacht das dann versendet werden kann

--- MMFT/DB/NachrichtenVerwalten.cs
+++ MMFT/DB/NachrichtenVerwalten.cs
@@ -3,6 +3,7 @@ using Modelle;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Windows;
 
 namespace MMFT.DB
@@ -22,6 +23,27 @@ namespace MMFT.DB
                 return;
             }
 
+            // Eigner Nutzer Datensatz
+            var eigenerNutzer = db.Nutzers.FirstOrDefault(n => n.Uuid == suuid);
+            if (eigenerNutzer == null)
+            {
+                MessageBox.Show("Eigener Nutzer nicht gefunden");
+                return;
+            }
+
+            // Empfaenger Datensatz
+            var empfaenger = db.Nutzers.FirstOrDefault(n => n.Uuid == euuid);
+            if (empfaenger == null)
+            {
+                MessageBox.Show("Empfänger nicht gefunden");
+                return;
+            }
+
+            // Verschluesselung für DB
+            string tInhaltDb = RsaHelfer.VerschluesselText(tInhalt, eigenerNutzer.PublicKey);
+            byte[] dInhaltDb = RsaHelfer.VerschluesselBytes(dInhalt, eigenerNutzer.PublicKey);
+
+
             long zeitstempel = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
             var nachricht = new Nachrichten
@@ -30,23 +52,30 @@ namespace MMFT.DB
                 EUuid = euuid,
                 SUuid = suuid,
                 Zeitstempel = zeitstempel,
-                TInhalt = tInhalt,
-                DInhalt = dInhalt
+                TInhalt = tInhaltDb,
+                DInhalt = dInhaltDb
 
             };
 
             db.Nachrichtens.Add(nachricht);
             db.SaveChanges();
 
+            // Verschluesselung für TCP
+            string tInhaltTcp = RsaHelfer.VerschluesselText(tInhalt, empfaenger.PublicKey);
+            byte[] dInhaltTcp = RsaHelfer.VerschluesselBytes(dInhalt, empfaenger.PublicKey);
+
+
             var paket = new PaketTyp2
             {
                 EUuid = euuid,
                 SUuid = suuid,
                 Zeitstempel = zeitstempel,
-                TInhalt = tInhalt,
-                DInhalt = dInhalt
+                TInhalt = tInhaltTcp,
+                DInhalt = dInhaltTcp
             };
+
             // sendeNachrichtTyp2TCP(paket);
+
         }
         // Typ 2 Nachrichten werden hier in der DB gespeichert. Werden direkt von dem TCP Dings geholt
         /*public static void SpeichereNachrichtEmpfangen(PaketTyp2 paket)
