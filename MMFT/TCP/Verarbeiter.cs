1a0734a6a1ed7f070a2b167164ad75ce0e513530
Milena Wagner <milena.wagner.steinberg@gmail.com>
2026-09-03 13:15:08 +0200

    Delete: Methodenkörper, die nochmal neu gemacht werden müssen. Added: Sende Paket Methode für jeden PaketTyp

--- MMFT/TCP/Verarbeiter.cs
+++ MMFT/TCP/Verarbeiter.cs
@@ -5,12 +5,14 @@ using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
+using MMFT.DB;
+using MMFT.DB.Models;
+
 
 namespace Verarbeiter
 {
     public class Verarbeiter
     {
-        private static string connectionString = "";
         
         //Methode, die den Header liest und weiterleitet
         public static void VerarbeiteEingehendesPaket(TcpClient client)
@@ -80,50 +82,22 @@ namespace Verarbeiter
             //Json wird in ein Objekt umgewandelt:
             PaketTyp1 paket = JsonSerializer.Deserialize<PaketTyp1>(jsonText);
             //Datenbank Verbindung öffnen
+            using var db = new MessengerDbContext();
 
-            using (SqliteConnection con = new SqliteConnection(connectionString))
-            {
-                con.Open();
-                string sql = @"Insert into Nutzer (UUID, Public_Key, Name, IP, P_Bild) values
-                                (@uuid, @pubkey, @name, @ip, @pBild)";
-                using (SqliteCommand cmd = new SqliteCommand(sql, con))
-                {
-                    cmd.Parameters.AddWithValue("@uuid", paket.Uuid);
-                    cmd.Parameters.AddWithValue("@pubkey", paket.PublicKey);
-                    cmd.Parameters.AddWithValue("@name", paket.Name);
-                    cmd.Parameters.AddWithValue("@ip", paket.Ip);
-                    cmd.Parameters.AddWithValue("@pBild", (object)paket.PBild ?? DBNull.Value);
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            Console.WriteLine($"Kontakt {paket.Name} gespeichert!");
+            
+            
         }
 
         private static void SpeicherTyp2(string jsonText) {
             //Json in Objekt umwandeln
             PaketTyp2 paket = JsonSerializer.Deserialize<PaketTyp2>(jsonText);
 
-            using (SqliteConnection con = new SqliteConnection(connectionString))
-            {
-                con.Open();
-                string sql = @"Insert into Nachrichten (E_UUID, S_UUID, Zeitstempel, T_Inhalt, D_Inhalt)
-                              values (@eUuid, @sUuid, @zeitstempel, @tInhalt, @dInhalt";
-                using (SqliteCommand cmd = new SqliteCommand(sql, con))
-                {
-                    cmd.Parameters.AddWithValue("@eUuid", paket.EUuid);
-                    cmd.Parameters.AddWithValue("@sUuid", paket.SUuid);
-                    cmd.Parameters.AddWithValue("@zeitstempel", paket.Zeitstempel);
-                    cmd.Parameters.AddWithValue("@tInhalt", (object)paket.TInhalt ?? DBNull.Value); //Wenn nichts geschickt wird, soll 
-                    cmd.Parameters.AddWithValue("@dInhalt", (object)paket.DInhalt ?? DBNull.Value); //"Null" in die DB geschrieben werden
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            Console.WriteLine("Nachricht gespeichert!");
+            
         }
         private static void VerarbeiteTyp3(string jsonText) {
             //Json in Objekt umwandeln
             PaketTyp3 paket = JsonSerializer.Deserialize<PaketTyp3>(jsonText);
-            Console.WriteLine($"Sync-Anfrage von UUID {paket.Uuid} erhalten");
+            
         }
     }
 }
